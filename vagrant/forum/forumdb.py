e40bc450c0f981cd121ba134570fffae8d13b01a
Mads Jensen <jensen.mn@gmail.com>
2016-08-14 22:36:57 +0100

    added the forums code

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,14 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
+import bleach
+import psycopg2
 
 ## Database connection
-DB = []
+DB = psycopg2.connect("dbname=forum")
+cursor = DB.cursor()
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,9 +19,16 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
+    cursor.execute("select time, content from posts order by time desc;")
+
+    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in cursor.fetchall())
+    #posts = bleach.clean(posts)
+    return posts
+    '''
     posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
+    '''
 
 ## Add a post to the database.
 def AddPost(content):
@@ -27,5 +37,13 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    content = bleach.clean(content)
+    cursor.execute("insert into posts (content) values (%s) ", (content,))
+    cursor.execute("delete from posts where content = 'cheese'")
+    DB.commit()
+
+    '''
     t = time.strftime('%c', time.localtime())
     DB.append((t, content))
+    '''
+    return
